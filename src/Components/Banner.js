a7ec46e79202f096bc6c3d113bb8040517c1eb4d
SAYEM5522 <md1040582@gmail.com>
2021-07-27 17:43:09 +0600

    banner text add

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,7 +3,19 @@ import "./Banner.css"
 const Banner = () => {
   return (
     <div className="banner">
+
+      <div>
+      <h1 className="banner__text">Start booking artists.</h1>
+      <h3 className="banner__descriptions">BeatGig is the easiest way to book talent for private parties, colleges, bars, weddings & more.</h3>
+      <div className="banner__auth">
+      <h3 className="banner__auth__text1" >Sign Up</h3>
+      <h3 className="banner__auth__text2">Log In</h3>
+      </div>
+      </div>
+      <div className="banner__info" >
       
+      </div>
+    
     </div>
   )
 }
